812f7fdcd2563dbce8ed0578a0265a523a83ed8f
Tushar Pathak <tusharpathak2503@gmail.com>
2023-06-07 21:37:35 +0530

    font and color changed

--- src/components/Spaces.js
+++ src/components/Spaces.js
@@ -12,63 +12,7 @@ function Spaces() {
   const [desc, setDesc] = useState('');
   const [code, setCode] = useState('');
   const [zindex, setZindex] = useState(-1);
-  // return (
-  //   <>
-  //     <Navbar/>
-  //     <StyledSpace>
-  //     <button className='button' style={{"background-color": "blue"}} onClick={()=>{setVisibility("visible"); setZindex(2)}}> + Create Space</button>
-  //     <button className='button'>Join</button>
-  //     <div>
-
-
-  //     <form style={{"visibility":visibility}}>
-  //       <div className='formdiv'>
-  //       <h1>Create Space</h1>
-  //           <div className='formelements'>
-  //               <div className='innerdiv'>
-  //                   <label>Name</label>
-  //                   <input name='name' type='text' onChange={(e) => {setName(e.target.value)}}></input>
-  //                   <label>Creator</label>
-  //                   <input type='text' name='creator' onChange={(e) => {setCreator(e.target.value)}}></input>
-  //               </div>
-  //           </div>
-
-  //           <div className='formelements'>
-  //               <div className='innerdiv'>
-  //                   <label>Subject</label>
-  //                   <input type='text' name='subject' onChange={(e) => {setSubject(e.target.value)}}></input>
-  //                   <label>Code</label>
-  //                   <input type ='text' name='code' onChange={(e) => {setCode(e.target.value)}}></input>
-  //               </div>
-  //           </div>
-  //           <div className='innerdiv'>
-  //               <label>Description</label>
-  //               <textarea style = {{"margin-left" : "10px", "margin-top":"10px", "background-color":"#ccff90", "font-size":"17px", "border-radius":"5px"}}type='text' name='desc' cols = "80" rows = "4" onChange={(e) => {setDesc(e.target.value)}}></textarea>
-  //           </div>
-  //           <br/>
-  //           <div style={{"margin-left":"auto", "margin-right":"auto"}}>
-  //           <button className = "createButton" type='submit'>Create</button>
-  //           <button className = "createButton" onClick={()=>setVisibility("hidden")}>Close</button>
-  //           </div>
-
-  //       </div>
-  //     </form>
-  //     <div className='container'>
-  //     <div className="card">
-  //       <h2>Study space</h2>
-  //       <p>teacher name</p>
-  //       <div className="space">description of the study space.</div>
-  //     </div>
-  //     <div className="card">
-  //       <h2>Study space</h2>
-  //       <p>teacher name</p>
-  //       <div className="space">description of the study space.</div>
-  //     </div>
-  //     </div>
-  //     </div>
-  //     </StyledSpace> 
-  //   </>
-  // )
+  
   if (visibility == 'visible') {
     return (
       <>
