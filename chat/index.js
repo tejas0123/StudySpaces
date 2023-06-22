939c6b55edff0e3249bdc8f3bcee88fdb1ae6da0
vishnu <vishnushedole09@gmail.com>
2023-06-22 18:28:24 +0530

    commit

--- chat/index.js
+++ chat/index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors:{
-        origin: "http://localhost:3000",
+        origin: "http://192.168.1.93:3000",
         methods:["GET", "POST"]
     }
 });
